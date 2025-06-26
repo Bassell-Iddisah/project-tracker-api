005e21102be5c0cc8d5c99e159ae5bdf9dd08392
Bassell-Iddisah <bbasssell16@gmail.com>
2025-06-26 03:27:24 +0000

    fix: Make UserMapper.toDTO non-static

--- src/main/java/com/gentleninja/mapper/UserMapper.java
+++ src/main/java/com/gentleninja/mapper/UserMapper.java
@@ -11,18 +11,18 @@ import java.util.stream.Collectors;
 @Component
 public class UserMapper {
 
-    public static UserDTO toDTO(User user) {
+    public UserDTO toDTO(User user) {
         if (user == null) return null;
 
-        Set<String> roleNames = user.getRoles().stream()
-                .map(role -> role.getName().toString())  // Convert RoleType to String
-                .collect(Collectors.toSet());
+//        Set<String> roleNames = user.getRoles().stream()
+//                .map(role -> role.getName().toString())  // Convert RoleType to String
+//                .collect(Collectors.toSet());
 
         return UserDTO.builder()
                 .id(user.getId())
                 .name(user.getName())
                 .email(user.getEmail())
-                .roles(roleNames)
+//                .roles(roleNames)
                 .build();
     }
 
