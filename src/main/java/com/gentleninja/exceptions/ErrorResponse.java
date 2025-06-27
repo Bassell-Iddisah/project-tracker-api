024a984b005528eac0a8dc10151960f08704095e
Bassell Iddisah <bbasssell16@gmail.com>
2025-06-27 01:02:28 +0000

    feat: Add ErrorResponse

---
+++ src/main/java/com/gentleninja/exceptions/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.gentleninja.exceptions;
+
+import java.time.LocalDateTime;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String path;
+}
